25ed97b0921edc431fe4925e56c00a4c9ef74184
VyacheslavSedyakin <chupa04011994@mail.ru>
2021-04-01 00:41:46 +0300

    Домашняя работа №1

---
+++ hw01/task1.js
@@ -0,0 +1,12 @@
+
+
+var celsius = prompt ('Введите температуру в градусах "Цельсия"', '');
+var fahrenheit = celsius * 1.8 + 32;
+
+alert  ( 'Температура в градусах по "Фаренгейту": ' + fahrenheit);
+
+var name = "«Василий»";
+var admin = "«Василий»";
+
+alert(admin);
+
